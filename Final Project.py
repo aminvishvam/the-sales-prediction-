a2a3c1f9c8cd53c2e2ceebf03c48ab08554e226e
Vishvam Amin <32908564+aminvishvam@users.noreply.github.com>
2019-02-09 19:43:52 +0530

    Add files via upload

---
+++ Final Project.py
@@ -0,0 +1,48 @@
+from project import app
+
+app.run(port=8838)
+
+##@app.route('/')
+##def hello_world():
+  ##  return render_template('admin/index.html')
+
+
+##@app.route('/addCategory')
+##def addCategory():
+  ##  return render_template('admin/addCategory.html')
+
+
+##@app.route('/addSubcategory')
+##def viewSubcategory():
+  ##  return render_template('admin/addSubcategory.html')
+
+
+##@app.route('/viewReatiler')
+##def viewReatiler():
+  ##  return render_template('admin/viewReatiler.html')
+
+
+##@app.route('/viewDistributor')
+##def viewDistributor():
+  ##  return render_template('admin/viewDistributor.html')
+
+
+##@app.route('/viewProduct')
+####  return render_template('admin/viewProduct.html')
+
+
+##@app.route('/addProduct')
+##def addProduct():
+##    return render_template('admin/addProduct.html')
+
+
+##@app.route('/viewCategory')
+##def viewCategory():
+  ##  return render_template('admin/viewCategory.html')
+
+
+##@app.route('/viewSubategory')
+##def viewSubategory():
+  ##  return render_template('admin/viewSubcategory.html')
+
+
